32756007d5b6fa542de1006b33e83c40ac9c39d4
Hoang Yen Nhi <yennhi03013009@gmail.com>
2025-05-13 13:26:51 +0700

    Add Player class

--- engine.py
+++ engine.py
@@ -15,8 +15,47 @@ class Ship:
         elif self.orientation == "v":
             return [start_index + i*10 for i in range(self.size)]
 
-s = Ship(size = 3)
-print(s. row)
-print(s.col)
-print(s.orientation)
-print(s.indexes)
+class Player:
+    def __init__(self):
+        self.ships = []
+        self.search = ["U" for i in range (100)]
+        self.place_ships(sizes =  [5, 4, 3, 3, 2])
+        list_of_lists = [ship.indexes for ship in self.ships]
+        self.indexes = [index for sublist in list_of_lists for index in sublist]
+    
+    def place_ships(self, sizes):
+        for size in sizes:
+            placed = False
+            while not placed:
+                #create new ship
+                ship = Ship(size)
+                #check possible
+                possible = True
+                for i in ship.indexes:
+                    #indexes must be < 100
+                    if i >= 100:
+                        possible = False
+                        break
+                    #ships cant behave like snake in snake game
+                    new_row = i//10
+                    new_col = i%10
+                    if new_row != ship.row and new_col != ship.col:
+                        possible = False
+                        break
+                    for other_ship in self.ships:
+                        if i in other_ship.indexes:
+                            possible = False
+                            break
+                #place the ship
+                if possible:
+                    self.ships.append(ship)
+                    placed = True
+
+
+    def show_ships(self):
+        indexes  = ["-" if i not in self.indexes else "X" for i in range (100)]
+        for row in range(10):
+            print(" ". join(indexes[(row-1)*10:row*10]))
+
+p = Player()
+p.show_ships()
